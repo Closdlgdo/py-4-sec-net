691d3b506d5233276ee6f1fb0689a94110323b70
Closdlgdo <cidstp031921@gmail.com>
2024-03-30 05:04:14 -0700

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/lists.py
+++ Chapter1/Working with Python Scripting/lists.py
@@ -3,6 +3,9 @@
 # Example:
 response = [200, 400, 403, 404, 500]
 
+# Indexes are used to access an element of a list. An index is an integer that indicates the position
+# of an element in a list. The first element of a list always starts at index 0.
+
 print(response[0])
 print(response[1])
 print(response[2])
