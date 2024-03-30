798b6c5c8776e223a4085eb5a12466abbe8ed407
Closdlgdo <cidstp031921@gmail.com>
2024-03-30 05:03:41 -0700

    Chapter 1 - Working with Python Scripting

---
+++ Chapter1/Working with Python Scripting/lists.py
@@ -0,0 +1,11 @@
+# Lists in Python are, used to store sets of related items of the same or different types. Also, a list is a mutable
+# data structure which allows the list content can be modified after it has been created.
+# Example:
+response = [200, 400, 403, 404, 500]
+
+print(response[0])
+print(response[1])
+print(response[2])
+print(response[3])
+print(response[4])
+print(response)
