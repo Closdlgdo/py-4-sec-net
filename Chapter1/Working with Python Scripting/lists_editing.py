443b3a6bc7478355ee48be5e741b97b0040dcbb5
Closdlgdo <cidstp031921@gmail.com>
2024-03-31 06:12:22 -0700

    Chapter 1 - Working with Python Scripting

---
+++ Chapter1/Working with Python Scripting/lists_editing.py
@@ -0,0 +1,12 @@
+# create a list using the append() method by adding objects, printing the objects, and then sorting them before printing
+# again. We describe a list of protocols in the following example, and use the key methods of a Python list, such as
+# add, index, and remove.
+
+protocols = []
+
+protocols.append("HTTP")
+protocols.append("SSH")
+protocols.append("Telnet")
+protocols.append("SMTP")
+
+print(protocols)
