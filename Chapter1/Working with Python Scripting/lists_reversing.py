40d720d75c995987b6c41edda5786866800471b6
Closdlgdo <cidstp031921@gmail.com>
2024-04-03 14:14:28 +0300

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/lists_reversing.py
+++ Chapter1/Working with Python Scripting/lists_reversing.py
@@ -1,2 +1,4 @@
 # Another interesting operation that we perform in lists is the one that offers the possibility of getting elements in a
 # reverse way in the list through the reverse() method.
+
+my_list = [1, 2, 3, 4, 5]
