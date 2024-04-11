f3266fc8abe3bb01f1acc9450878fe1108515031
Closdlgdo <cidstp031921@gmail.com>
2024-04-11 13:07:56 +0300

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Dictionaries/dictionaries_002.py
+++ Chapter1/Working with Python Scripting/Dictionaries/dictionaries_002.py
@@ -15,3 +15,10 @@ print(len(my_dict))
 # We can use the keys() method to get a list of all the keys in the dictionary.
 
 print(my_dict.keys())
+
+# The dict class implements three methods, since they return an iterable data type, known as view objects. These objects
+# provide a view of the keys and values of type dict_values contained in the dictionary, and if the dictionary changes,
+# these objects are instantly updated. The methods are as follows:
+# • items(): Returns a view of (key, value) pairs from the dictionary.
+# • keys(): Returns a view of the keys in the dictionary.
+# • values(): Returns a view of the values in the dictionary.
