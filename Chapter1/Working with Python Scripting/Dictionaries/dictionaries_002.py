1d1bf7f0af8df6c8a39fd5be9c655fde0c72b852
Closdlgdo <cidstp031921@gmail.com>
2024-04-11 13:07:07 +0300

    Chapter 1 - Working with Python Scripting

---
+++ Chapter1/Working with Python Scripting/Dictionaries/dictionaries_002.py
@@ -0,0 +1,5 @@
+# Each key must be unique: That means it is not possible to have more than one key of the same value.
+# • A key may be a number or a string.
+# • A dictionary is not a list: A list contains a set of numbered values, while a dictionary
+# holds pairs of values.
+# • The len() function: This works for dictionaries and returns the number of key-value elements in the dictionary.
