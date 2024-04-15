fa8e124365ad34b8149cf63717ec851be1f9a27f
Closdlgdo <cidstp031921@gmail.com>
2024-04-15 20:54:53 +0300

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Classes/welcome_to_the_class.py
+++ Chapter1/Working with Python Scripting/Classes/welcome_to_the_class.py
@@ -6,3 +6,13 @@ class Protocol(object):
 
     def getProtocolInfo(self):
         return self.name+ " "+str(self.number)+ " "+self.description
+
+
+# The init method is a special method that acts as a constructor method to perform the necessary initialization
+# operation. The method’s first parameter is a special keyword, and we use the self-identifier for the current object
+# reference. The self keyword is a reference to the object itself and provides a way for its attributes and methods to
+# access it.
+# The constructor method must provide the self parameter and may have more parameters than just self; if this happens,
+# the way in which the class name is used to create the object must reflect the __init__ definition. This method is used
+# to set up the object, in other words, to properly initialize its internal state. This parameter is equivalent to the
+# pointer that can be found in languages such as C ++ or Java.
