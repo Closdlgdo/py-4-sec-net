4cff43188e4e9b8419163df69dc24c4b5d80fdd3
Closdlgdo <cidstp031921@gmail.com>
2024-04-15 20:52:30 +0300

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Functinos/working_with_funcs.py
+++ Chapter1/Working with Python Scripting/Functinos/working_with_funcs.py
@@ -14,3 +14,9 @@ diction = {1:100,2:200,3:300}
 print(contains(diction,200))
 print(contains(diction,300))
 print(contains(diction,350))
+
+# Two important factors make parameters special:
+# • Parameters only exist within the functions in which they were described, and the only place where the parameter can
+# be specified is in the space between a pair of parentheses in the def state.
+# • Assigning a value to the parameter is done at the time of the function’s invocation by specifying the corresponding
+# argument.
