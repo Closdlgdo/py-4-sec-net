86cf2f33fa380728476ea6a264205388c0c65920
Closdlgdo <cidstp031921@gmail.com>
2024-04-17 21:27:07 +0300

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Inheritance/single_inheritance.py
+++ Chapter1/Working with Python Scripting/Inheritance/single_inheritance.py
@@ -2,3 +2,12 @@
 # following is an example of simple inheritance in Python where we have a base class and a child class that inherits
 # from the parent class. Note the presence of the __init__ method in both classes , which allows you to initialize the
 # properties of the class as an object constructor.
+
+class ParentClass:
+    def __init__(self):
+        pass
+
+
+class ChildClass(ParentClass):
+    def __init__(self):
+        super().__init()
