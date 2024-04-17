133b6978f6d2a5f1961d4f30dc5555f6007eb33f
Closdlgdo <cidstp031921@gmail.com>
2024-04-17 21:29:36 +0300

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Inheritance/single_inheritance.py
+++ Chapter1/Working with Python Scripting/Inheritance/single_inheritance.py
@@ -3,11 +3,21 @@
 # from the parent class. Note the presence of the __init__ method in both classes , which allows you to initialize the
 # properties of the class as an object constructor.
 
-class ParentClass:
-    def __init__(self):
-        pass
+class BaseClass:
+    def __init__(self, property):
+        self.property = property
+
+    def message(self):
+        print('Welcome to Base Class')
+
+    def message_base_class(self):
+        print('This is a message from Base Class')
 
 
-class ChildClass(ParentClass):
-    def __init__(self):
-        super().__init()
+class ChildClass(BaseClass):
+    def __init__(self, property):
+        BaseClass.__init__(self, property)
+
+    def message(self):
+        print('Welcome to ChildClass')
+        print('This is inherited from BaseClass')
