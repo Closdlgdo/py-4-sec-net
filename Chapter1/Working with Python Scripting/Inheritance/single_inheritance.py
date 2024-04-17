0c91149a446cdb00c55dc0243d36c2614e634ea1
Closdlgdo <cidstp031921@gmail.com>
2024-04-17 21:26:19 +0300

    Chapter 1 - Working with Python Scripting

---
+++ Chapter1/Working with Python Scripting/Inheritance/single_inheritance.py
@@ -0,0 +1,4 @@
+# Single inheritance occurs when a child class inherits the attributes and methods of a single parent class. The
+# following is an example of simple inheritance in Python where we have a base class and a child class that inherits
+# from the parent class. Note the presence of the __init__ method in both classes , which allows you to initialize the
+# properties of the class as an object constructor.
