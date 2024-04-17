9176ae5df6f1a94b0930087ca7213159f0042d34
Closdlgdo <cidstp031921@gmail.com>
2024-04-17 21:28:05 +0300

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Inheritance/multiple_inheritance.py
+++ Chapter1/Working with Python Scripting/Inheritance/multiple_inheritance.py
@@ -1,3 +1,14 @@
 # Multiple inheritance occurs when a child class inherits attributes and methods from more than one parent class. We
 # could separate both main classes with a comma when creating the secondary class. In the following example we are
 # implementing multiple inheritance where the child class is inheriting from the MainClass and MainClass2 classes.
+
+class MainClass:
+    pass
+
+
+class MainClass2:
+    pass
+
+
+class SecondaryClass(MainClass, MainClass2):
+    pass
