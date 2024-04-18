6a85352fdd7d847a0a39a1ae5ab4d9c2d08b539b
Closdlgdo <cidstp031921@gmail.com>
2024-04-18 12:25:24 +0300

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Inheritance/multiple_inheritance.py
+++ Chapter1/Working with Python Scripting/Inheritance/multiple_inheritance.py
@@ -3,12 +3,29 @@
 # implementing multiple inheritance where the child class is inheriting from the MainClass and MainClass2 classes.
 
 class MainClass:
-    pass
+    def message_main(self):
+        print('Welcome to Main Class')
 
 
 class MainClass2:
-    pass
+    def message_main2(self):
+        print('Welcome to Main Class2')
 
 
-class SecondaryClass(MainClass, MainClass2):
-    pass
+class ChildClass(MainClass,MainClass2):
+    def message(self):
+        print('Welcome to ChildClass')
+        print('This is inherited from MainClass and MainClass2')
+
+
+if __name__ == '__main__':
+    child_obj = ChildClass()
+    child_obj.message()
+    child_obj.message_main()
+    child_obj.message_main2()
+
+# Output:
+# Welcome to ChildClass
+# This is inherited from MainClass and MainClass2
+# Welcome to Main Class
+# Welcome to Main Class2
