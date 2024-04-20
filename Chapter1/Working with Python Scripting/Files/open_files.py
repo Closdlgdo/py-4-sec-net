06b02ea13a3e64a8cecd3e95aa8942dd9088d63d
Closdlgdo <cidstp031921@gmail.com>
2024-04-20 12:03:11 +0300

    Chapter 1 - Working with Python Scripting

---
+++ Chapter1/Working with Python Scripting/Files/open_files.py
@@ -0,0 +1,6 @@
+# The open() function is usually used with two parameters (the file with which we are going to work and the access mode)
+# and it returns a file-type object. When opening a file with a certain access mode with the open() function, a file
+# object is returned.
+# The opening modes can be r (read), w (write), and a (append). We can combine the previous modes with others depending
+# on the file type. We can also use the b (binary), t (text), and + (open reading and writing) modes. For example, you
+# can add a + to your option, which allows read/ write operations with the same object:
