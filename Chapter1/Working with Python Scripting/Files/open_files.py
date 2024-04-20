7df8d94d119577c67c9449fa3e457b8252904234
Closdlgdo <cidstp031921@gmail.com>
2024-04-20 12:03:49 +0300

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Files/open_files.py
+++ Chapter1/Working with Python Scripting/Files/open_files.py
@@ -4,3 +4,4 @@
 # The opening modes can be r (read), w (write), and a (append). We can combine the previous modes with others depending
 # on the file type. We can also use the b (binary), t (text), and + (open reading and writing) modes. For example, you
 # can add a + to your option, which allows read/ write operations with the same object:
+file = open("example.txt", "w+")
