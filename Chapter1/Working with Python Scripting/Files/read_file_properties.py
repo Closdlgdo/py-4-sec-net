799fef7f718885c1629cb09504c9a834858a3270
Closdlgdo <cidstp031921@gmail.com>
2024-04-21 23:08:54 +0300

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Files/read_file_properties.py
+++ Chapter1/Working with Python Scripting/Files/read_file_properties.py
@@ -4,3 +4,10 @@ print("Name: "+file_descryptor.name)
 print("Mode: "+file_descryptor.mode)
 print("Encoding: "+str(file_descryptor.encoding))
 file_descryptor.close()
+
+# When reading a file, the readlines() method reads all the lines of the file and joins them in a list sequence. This
+# method is very useful if you want to read the entire file at once:
+
+print("Lines: ")
+for line in file_descryptor.readlines():
+    print(line.strip())
