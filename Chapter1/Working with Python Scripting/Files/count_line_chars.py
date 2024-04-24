0de98f8d8ec99d02777b6e68cc5093f57e250fd5
Closdlgdo <cidstp031921@gmail.com>
2024-04-24 13:33:36 -0700

    Chapter 1 - Working with Python Scripting

---
+++ Chapter1/Working with Python Scripting/Files/count_line_chars.py
@@ -0,0 +1,16 @@
+# In the following example, we are using the readlines() method to process the file and get counts of the lines and
+# characters in this file.
+
+try:
+    countlines = countchars = 0
+    file = open('count_lines_chars.py', 'r')
+    lines = file.readlines()
+    for line in lines:
+        countlines += 1
+        for char in line:
+    file.close()
+    print("Characters in file:", countchars)
+    print("Lines in file:", countlines)
+except IOError as error:
+    print("I/O error occurred:", str(error))
+
