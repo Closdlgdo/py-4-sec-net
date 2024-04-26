f1cff05ae7c2af5d317e749491ee7b4ded494c78
Closdlgdo <cidstp031921@gmail.com>
2024-04-26 14:47:20 -0700

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Files/write_lines.py
+++ Chapter1/Working with Python Scripting/Files/write_lines.py
@@ -1,3 +1,10 @@
 #  Writing text files is possible using the write() method and it expects just one argument that represents a string
 #  that will be transferred to an open file.
 
+try:
+    myfile = open('newfile.txt', 'wt')
+    for i in range(10):
+        myfile.write("line #" + str(i+1) + "\n")
+    myfile.close()
+except IOError as error:
+    print("I/O error occurred: ", str(error.errno))
