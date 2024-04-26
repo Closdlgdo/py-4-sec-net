13b9d56e73f8c652b7ec25162bd49a8ca735e62b
Closdlgdo <cidstp031921@gmail.com>
2024-04-26 14:48:18 -0700

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Files/write_lines.py
+++ Chapter1/Working with Python Scripting/Files/write_lines.py
@@ -11,3 +11,9 @@ except IOError as error:
 
 # The write() method is very useful if we want to write a string to a file at a certain location.
 myfile.write("This is a new line being added to the file.\n")
+# If we want to write multiple lines, we can use the writelines() method. This method takes a list of strings as an
+
+# example:
+with open('newfile.txt', 'a') as myfile:
+    lines = ["New line 1\n", "New line 2\n", "New line 3\n"]
+    myfile.writelines(lines)
