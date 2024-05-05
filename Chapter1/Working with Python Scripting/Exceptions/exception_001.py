44433d6b678caa328f2440327139efa5b8b9f288
Closdlgdo <cidstp031921@gmail.com>
2024-05-05 06:33:06 -0700

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Exceptions/exception_001.py
+++ Chapter1/Working with Python Scripting/Exceptions/exception_001.py
@@ -3,3 +3,10 @@
 
 # "4" + 4
 # TypeError: can only concatenate str (not "int") to str
+
+try:
+    print("10/0=", str(10/0))
+except Exception as exception:
+    print("Error =", str(exception))
+
+# Error = division by zero
