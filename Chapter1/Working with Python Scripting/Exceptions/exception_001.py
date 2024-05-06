6a5f590055fdc63e5baba5ef9931389a9beb76b0
Closdlgdo <cidstp031921@gmail.com>
2024-05-06 05:30:04 -0700

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Exceptions/exception_001.py
+++ Chapter1/Working with Python Scripting/Exceptions/exception_001.py
@@ -13,3 +13,16 @@ except Exception as exception:
 
 # The try keyword begins a block of the code that may or may not be performing correctly. Next, Python tries to perform
 # some operations; if it fails, an exception is raised, and Python starts to look for a solution.
+
+# At this point, the except keyword starts a piece of code that will be executed if anything inside the try block goes
+# wrong – if an exception is raised inside a previous try block, it will fail here, so the code located after the except
+# keyword should provide an adequate reaction to the raised exception. The following code raises an exception related to
+# accessing an element that does not exist in the list:
+
+try:
+    list = []
+    element = list[0]
+except Exception as exception:
+    print("Error =", str(exception))
+
+# Error = list index out of range
