0807644e2fda05f9d3cf62e74f8e9a7fade81126
Closdlgdo <cidstp031921@gmail.com>
2024-05-11 06:29:06 -0700

    Chapter 1 - Working with Python Scripting

--- Chapter1/Working with Python Scripting/Exceptions/exception_tree_001.py
+++ Chapter1/Working with Python Scripting/Exceptions/exception_tree_001.py
@@ -15,3 +15,11 @@ printExceptionsTree(BaseException)
 
 # • A point inside the tree from which we start traversing the tree
 # • A level to build a simplified drawing of the tree’s branches
+
+# In the output of the previous script, we can see the root of Python’s exception classes is the BaseException class
+# (this is a superclass of all the other exceptions). For each of the encountered classes, it performs the following
+# set of operations:
+
+# • Print its name, taken from the __name__ property.
+# • Iterate through the list of subclasses delivered by the __subclasses__() method, an recursively invoke the
+# printExceptionsTree() function, incrementing the nesting level, respectively.
